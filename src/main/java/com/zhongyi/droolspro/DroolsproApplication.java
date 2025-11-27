57eb0f3f26c2cb59af6b04c4b4a177ded9da927f
fengjutian <fjutian@foxmail.com>
2025-11-27 14:03:06 +0800

    first commit

---
+++ src/main/java/com/zhongyi/droolspro/DroolsproApplication.java
@@ -0,0 +1,13 @@
+package com.zhongyi.droolspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhongyi/droolspro/DroolsproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhongyi.droolspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
